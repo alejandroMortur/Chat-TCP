76791c54a726f9a7615dd5c4193b56146c121af7
alex <moronturiealejandro@gmail.com>
2024-01-24 13:23:00 +0100

    prueba commit inicial en rama v1-1

--- src/Formulario.java
+++ src/Formulario.java
@@ -5,16 +5,21 @@ import java.awt.event.ActionListener;
 public class Formulario  extends  JFrame{
 
     public JPanel panel_principal;
-    private JTextField campo_nombre;
+    private JTextField campo_contraseña;
     private JLabel etiqueta_nombre;
     private JButton pulsa_registro;
+    private JLabel etiqueta_contraseña;
+    private JTextField campo_nombre;
 
-        public Formulario() {
+    public Formulario() {
             pulsa_registro.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
 
-                    JOptionPane.showMessageDialog(pulsa_registro, "!hola " + campo_nombre.getText());
+                    String nombre = campo_nombre.getText();
+                    String contraseña = campo_contraseña.getText();
+
+                    JOptionPane.showMessageDialog(pulsa_registro, "Datos obtenidos: Nombre: "+nombre+", Contraseña: "+contraseña);
 
                 }
 
